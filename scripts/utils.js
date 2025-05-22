b3e6a542d4d735e99f17895c16a495a96ba4991d
Rafael3323 <rafael.albuquerque1@outlook.com>
2025-05-22 20:00:22 -0300

    fix

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,16 +1,23 @@
 document.querySelectorAll('.profileContainer > a').forEach(link => {
-    link.addEventListener('click', function(e) {
+    link.addEventListener('mouseover', function(e) {
         e.preventDefault();
 
         // Fecha outros submenus
         document.querySelectorAll('.submenu').forEach(menu => {
             if (menu !== this.nextElementSibling) {
                 menu.style.display = 'none';
+
             }
         });
 
         // Toggle submenu atual
         const submenu = this.nextElementSibling;
         submenu.style.display = submenu.style.display === 'flex' ? 'none' : 'flex';
+    });
+    // fecha ao tirar o mouse de cima
+    link.addEventListener('mouseout', function(e) {
+        e.preventDefault();
+        const submenu = this.nextElementSibling;
+        submenu.style.display = 'none';
     });
 });
